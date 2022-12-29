15d65fc9e1f32158ea97007b2f37f8a1f57ccab1
genomewalker <genomewalker@gmail.com>
2022-12-29 23:48:21 +0100

    Minor

--- aMGSIM/library/functions.py
+++ aMGSIM/library/functions.py
@@ -341,9 +341,7 @@ def _genome_size(x):
 
 def load_read_length_freqs(file):
     file = ujson.load(file)
-    print(len(file[0]))
-    exit()
-    return reduce(lambda d, src: d.update(src) or d, file[0], {})
+    return reduce(lambda d, src: d.update(src) or d, fast_flatten(file), {})
 
 
 def get_compression_type(filename):
