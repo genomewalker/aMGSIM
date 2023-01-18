6f6e6ab38144ab5f3d7a485a60edc6132bb2a041
genomewalker <genomewalker@gmail.com>
2023-01-18 23:19:41 +0100

    Minor

--- aMGSIM/library/pa_functions.py
+++ aMGSIM/library/pa_functions.py
@@ -415,7 +415,7 @@ def analyze_proteins(x, files, gene_predictions, min_len, outdir, debug, nproc):
         # Get reads
         df_reads = pr.PyRanges(df)
         print(genome)
-        print(df_reads.df["Chromosome"])
+        print(df_reads.df["name"])
         exit()
 
         if df_reads.df["Chromosome"].str.contains(genome).any():
